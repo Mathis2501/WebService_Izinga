d54be3bfa072cb2145a7559c03a0a5ba4281db5c
UrbanSkurk <UrbanSkurk@HELL>
2017-10-04 12:19:42 +0200

    Stuff is done.

--- WebService_Izinga/WebService_Izinga/LogReader.cs
+++ WebService_Izinga/WebService_Izinga/LogReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WebService_Izinga
@@ -14,6 +15,10 @@ namespace WebService_Izinga
         {
             TeliaSMSs = new List<TeliaSMS>();
         }
+
+        //Intended to split up a string into object type: DateTime
+        //string format is supposed to be "2017-10-04 10:51:57"
+
         public void TeliaLogReader()
         {
             TeliaSMS NewSMS = new TeliaSMS();
@@ -25,51 +30,71 @@ namespace WebService_Izinga
 
             File.Copy(filepath, targetpath);
 
-            StreamReader sr = new StreamReader(targetpath);
 
             foreach (string line in File.ReadAllLines(targetpath))
             {
                 if (line.Contains("Number=+4551418279"))
                 {
-                    string[] lines = line.Split(';');
-
-                    for (int i = 0; i < lines.Length; i++)
-                    {
-                        if (i == 0)
-                        {
-                            string[] DateandTime = lines[i].Split(' ');
-                            for (int j = 0; j < DateandTime.Length; j++)
-                            {
-                                if (j == 0)
-                                {
-                                    DateandTime[j] = DateandTime[j].Substring(1);
-                                    string[] RealDateandTime = DateandTime[j].Split('-');
-                                    for (int k = 0; k < RealDateandTime.Length; k++)
-                                    {
-                                        if (k == 0)
-                                        {
-                                            //NewSMS.time = RealDateandTime[k];
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    Regex regex = new Regex(@"\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}[:]\d{2}");
+                    Match match = regex.Match(line);
+                    Console.WriteLine(match.Value);
                 }
             }
-
-            sr.Dispose();
-
-
+      
 
             Console.ReadKey();
 
 
             File.Delete(targetpath);
-
-
-
+        }
+        public DateTime IntepretDateTime(string input)
+        {
+            string[] date, time, dateAndTime;
+            dateAndTime = input.Split(' ');
+            date = dateAndTime[0].Split('-');
+            time = dateAndTime[1].Split(':');
+            return new DateTime(int.Parse(date[0]), int.Parse(date[1]), int.Parse(date[2]), int.Parse(time[0]), int.Parse(time[1]), int.Parse(time[2]));
 
         }
+
+        //public void OldLogReader()
+        //{
+            //StreamReader sr = new StreamReader(targetpath);
+            //foreach (string line in File.ReadAllLines(targetpath))
+            //{
+            //    if (line.Contains("Number=+4551418279"))
+            //    {
+            //        string[] lines = line.Split(';');
+
+            //        for (int i = 0; i < lines.Length; i++)
+            //        {
+            //            if (i == 0)
+            //            {
+            //                string[] DateandTime = lines[i].Split(' ');
+            //                for (int j = 0; j < DateandTime.Length; j++)
+            //                {
+            //                    if (j == 0)
+            //                    {
+            //                        DateandTime[j] = DateandTime[j].Substring(1);
+            //                        string[] RealDateandTime = DateandTime[j].Split('-');
+            //                        for (int k = 0; k < RealDateandTime.Length; k++)
+            //                        {
+            //                            if (k == 0)
+            //                            {
+            //                                //NewSMS.time = RealDateandTime[k];
+            //                            }
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            //sr.Dispose();
+
+        //}
+
+
     }
 }
